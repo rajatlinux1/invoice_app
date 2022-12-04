2acd1208b53f8e8f803e6b90bcbee67d496f7556
Rajat Saini <103591089+rajatlinux1@users.noreply.github.com>
2022-12-04 23:31:55 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,15 @@
+data = [
+    ["Solar Plate", 250.0, 2],
+    ["Bettry", 355.0, 1],
+    ["Bulb", 99.9, 4],
+    ["led", 150, 8],
+]
+
+term_days = 30
+company_name = "West Repair Inc."
+web_site = "www.westernrepair.com"
+company_address = "X-27, Industrial Area,Okhla Industrial Estate,New Delhi, Delhi 110020"
+customer_address = "X-27, Industrial Area,Okhla Industrial Estate,New Delhi, Delhi 110020"
+customer_name = "CustomerName"
+customer_phone = +91-1234567890
+invoice_number = 12345
